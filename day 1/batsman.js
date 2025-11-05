970d54a788353e011d329fc4e2a3f4b033af1774
unknown <kumarkdd1122@gmail.com>
2025-11-05 10:47:13 +0530

    first commit

---
+++ day 1/batsman.js
@@ -0,0 +1,17 @@
+const batsman = ()=>{
+    console.log("itz executeed");
+    
+}
+   export const Batsman = (name)=>{
+    return (`Batsman:${name}`);
+   }
+
+   export const Bowler = (name)=>{
+      return (`Bowler:${name}`)
+   }
+   export const Fielder = (name)=>{
+      return(`fielder:${name}`)
+   }
+
+
+export default batsman;
